16853d6de55f1639ca0508e3dff9922a0c91d3af
Hank McGill <henrymcgill@gmail.com>
2023-05-31 08:55:31 -0400

    continued work on building layout

--- src/components/tsx/hero/hero.tsx
+++ src/components/tsx/hero/hero.tsx
@@ -4,15 +4,26 @@ import styles from './hero.module.css';
 export default component$(() => {
   return (
     <div class={['container', styles.hero]}>
-      <h1>
-        What is <span class="highlight">kafkaPraag</span>, exactly?
-      </h1>
-      <p>A monitoring tool for your Kafka cluster.  Get real-time updates and on-the-fly changes to your pubSub model with kafkaPRAAG!</p>
-      <div class={styles['button-group']}>
-        <a href="https://qwik.builder.io/docs" target="_blank" class="button button-dark">
+    <article class={['container', styles.intro]}>
+      <div class={['container', styles.article]}>
+      <h1>Smth smth smth.</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil assumenda cum fuga eos ea rem est aperiam reiciendis commodi velit modi earum dicta facilis voluptas maxime, quidem laboriosam dignissimos ex?</p>
+
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil assumenda cum fuga eos ea rem est aperiam reiciendis commodi velit modi earum dicta facilis voluptas maxime, quidem laboriosam dignissimos ex?</p>
+
+      <button>
+        <a href="/docs">
+          Download
+        </a>
+      </button>
+      <button>
+        <a href="/docs">
           Quick start guide
         </a>
+      </button>
       </div>
+    <div class={['container', styles.imgbox]}>img here</div>
+    </article>
     </div>
   );
 });
